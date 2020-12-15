e2954d76f66d056dc9ceeeb5a9f39f359db238f5
yonikosiner <yoni@kosiner.co.uk>
2020-12-15 12:41:07 +0000

    don't want to foget to commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,23 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Container } from "react-bootstrap";
 import { Footer } from "./components/Footer";
 import { Header } from "./components/header";
 import { HomeScreen } from "./screens/homeScreen";
+import { ProductScreen } from "./screens/productScreen";
 
 function App() {
   return (
-    <>
+    <Router>
       <Header />
       <main className="py-3">
         <Container>
-          <HomeScreen />
+          <Route path="/" component={HomeScreen} exact />
+          <Route path="/product/:id" component={ProductScreen} exact />
         </Container>
       </main>
       <Footer />
-    </>
+    </Router>
   );
 }
 
