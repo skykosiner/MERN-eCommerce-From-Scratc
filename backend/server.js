22dcfb4eb61abbe871f40ace7f335946f281bd87
yonikosiner <yoni@kosiner.co.uk>
2020-12-16 11:29:25 +0000

    redux

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import colors from "colors";
 import connectDB from "./config/db.js";
 import productRoutes from "./routes/productRoute.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
 dotenv.config();
 
@@ -16,7 +17,11 @@ app.get("/", (req, res) => {
 
 app.use("/api/products", productRoutes);
 
-const PORT = process.env.POR || 5000;
+app.use(notFound);
+
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(
   PORT,
